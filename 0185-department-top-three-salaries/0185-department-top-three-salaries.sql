042e53121f0600ffade33a9807cb20c839d4543b
Swati <134836331+Snpyxd@users.noreply.github.com>
2025-12-03 20:23:39 +0530

    Time: 978 ms (40.91%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,18 @@
+SELECT 
+    d.Name AS Department,
+    e.Name AS Employee,
+    e.Salary
+FROM (
+    SELECT 
+        Name,
+        Salary,
+        DepartmentId,
+        DENSE_RANK() OVER (
+            PARTITION BY DepartmentId 
+            ORDER BY Salary DESC
+        ) AS rnk
+    FROM Employee
+) e
+JOIN Department d 
+    ON e.DepartmentId = d.Id
+WHERE e.rnk <= 3;
